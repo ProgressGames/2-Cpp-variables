1e8d8e9e4031ec5efd6bc6841c4a2f2f7b45df53
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-08-09 15:17:05 +0300

    discription and tips

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "Rus"); 
 	
-	double Age = 5, a = 1.9, b = 9.1;
+	double Age = 5, a = 1.9, b = 9.1; // это переменные  типа данных Double | these are variables of the Double data type
 
-	cout << Age << " " << a << endl;
+	cout << Age << " " << a << endl; // эта строка выводит данные которые храняться в переменных Age и a | this line outputs the data that is stored in the variables Age and a
 
 }
